abaf13980c91122b8647596286dc9a2fc8a31c16
Direcik <umutdemiorglu08@gmail.com>
2025-08-10 18:45:52 +0300

    first commit

--- company/urls.py
+++ company/urls.py
@@ -4,6 +4,7 @@ from company import views
 urlpatterns = [
     path('companydashboard/', views.companyDashboard, name="companydashboard"),
     path('addcompany/', views.addCompany, name="addcompany"),
+    path('api/get-company/', views.get_company_from_izibiz, name='get_company_from_izibiz'),
     path('updatecompany/<int:id>', views.updateCompany, name="updatecompany"),
     path('deletecompany/<int:id>', views.deleteCompany, name="deletecompany"),
 
